05aa77a82ae95497297f0ff2f6f5bb2bb81a2e1b
unknown <airalalainesegundo@gmail.com>
2025-01-31 08:35:03 +0800

    loop

---
+++ loop/js/math.js
@@ -0,0 +1,55 @@
+function sum() {
+    const num = document.getElementById("number").value;
+
+    const display = document.getElementById("display");
+
+    let answer = "";
+    let sum = 0;
+    for (let a = 1; a <= num; a++) {
+        answer += a + "<br/>";
+        sum += a;
+    }
+
+    answer += "The sum is" + sum;
+    display.innerHTML = answer;
+}
+
+
+function factorial() {
+
+
+    let number = document.getElementById('number').value;
+    let answer = '';
+    let product = number;
+    let num = number;
+    while (num > 1) {
+        answer += num + "<br/>";
+        product *= (num - 1);
+        num--;
+    }
+    answer += num + "<br/>"
+    answer += "The factorial of" + number + "is" + product;
+    document.getElementById('display').innerHTML = answer;
+}
+
+
+function odd() {
+    let num = document.getElementById("number").value;
+    let display = document.getElementById("display");
+    let answer = "";
+    for (let a = 1; a <= num; a +=2) {
+        answer += a + "<br>";
+    }
+    display.innerHTML = answer;     
+}
+
+function even() {
+    let num = document.getElementById("number").value;
+    let display = document.getElementById("display");
+    let answer = "";
+    for (let a = 2; a <= num; a +=2) {
+        answer += a + "<br>";
+    }
+    display.innerHTML = answer;  
+}
+
